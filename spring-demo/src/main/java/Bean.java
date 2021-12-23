d9a99c747d6a7e70445adbe4f7db4d8d8e9d4af1
jdzhao2 <jdzhao2@iflytek.com>
2021-12-23 11:30:32 +0800

    feat: first commit

---
+++ spring-demo/src/main/java/Bean.java
@@ -0,0 +1,9 @@
+/**
+ * @author jdzhao2
+ * @version V1.0
+ * @Description (这里用一句话描述这个类的作用)
+ * @date 2021.12.22 16:37
+ */
+public class Bean {
+
+}
---
+++ spring-demo/src/main/java/Test.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author jdzhao2
+ * @version V1.0
+ * @Description (这里用一句话描述这个类的作用)
+ * @date 2021.12.22 16:37
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		
+		context.register(Bean.class);
+		context.refresh();
+		System.out.println("spring-framework源码环境编译好了：" + context.getBean(Bean.class));
+	}
+}
